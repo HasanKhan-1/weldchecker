6cda37113543fd990e54323ab1b333221ef81925
Neil Patel <Neil.Patel@BRAMPTON.MARTINREA.COM>
2023-12-06 11:28:35 -0500

    haha hasan made booboo

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ current_hour = time.localtime().tm_hour
 
 tag_list = ['WeldReworkReasonCode', 'WeldReworkId', 'WeldTechLogged']
 reason_codes = {
-    "0": "Nothing",
-    "1": "Weld of location",
-    "2": "Weld burn through",
-    "3": "Weld Gap Issues",
-    "4": "Wire feeding issues",
-    "5": "Gap Issues/weld Porosity",
-    "6": "Bad tip/consumables",
-    "7": "Change of weld parameters"
+    0: "Nothing",
+    1: "Weld of location",
+    2: "Weld burn through",
+    3: "Weld Gap Issues",
+    4: "Wire feeding issues",
+    5: "Gap Issues/weld Porosity",
+    6: "Bad tip/consumables",
+    7: "Change of weld parameters"
 }
 
 t = time.localtime()
@@ -45,9 +45,10 @@ while True:
             with open(file_name, 'w', newline='') as csv_file:
                 csv_file.write("curr_time, WeldReworkId, WeldReworkReasonCode\n")
 
-        if ret[2]:  # Writes out if our WeldTechlogged is True
+        if ret[2].Value:  # Writes out if our WeldTechlogged is True
             print("This is my file name: %s", curr_time)
+            file_name = dir + '\\' + generateFilename(hour)
             with open(file_name, 'a') as csv_file:
-                csv_file.write(f"{curr_time}, {ret[1]}, {reason_codes[ret[0]]}\n")
+                csv_file.write(f"{curr_time}, {ret[1].Value}, {reason_codes[ret[0].Value]}\n")
 
-        time.sleep(1)
+        time.sleep(100)
