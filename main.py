3d9fae4435accbef994926957fc422dcc7a47353
Hasan <hasanmahmkhan@gmail.com>
2023-12-06 11:03:51 -0500

    hasan

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
 import time
 from pylogix import PLC
 from emailer import *
-
 
 current_hour = time.localtime().tm_hour
 
@@ -19,55 +17,37 @@ reason_codes = {
 }
 
 t = time.localtime()
-year = t.tm_year
-month = t.tm_mon
-day = t.tm_mday
-curr_time = str(t.tm_hour) +":"+str(t.tm_min)+ ":"+ str(t.tm_sec)
-print("The current time is: "+ curr_time+ " The date is: "+ str(year) +"/"+str(month)+"/"+ str(day))
+curr_time = str(t.tm_hour) + ":" + str(t.tm_min) + ":" + str(t.tm_sec)
+file_name = ""
 
-# Check if it's time to generate the file (every 8 hours starting at 7 am)
-
-if current_hour == 7 or (current_hour - 7) % 8 == 0:     
-  hour = int(time.strftime("%H"))
-  # make a new file 
-
-
-elif current_hour == 0:
-    # Send files
-#   server = initEmailServer()
-#   if server:
-#       sendEmail(server)
-#       server.quit()
-#       print("Files sent at", datetime.now())
-#   else:
-#       print("Email server connection failed")
-    print("dosomething")
-
-
-
-with PLC() as comm:
-    comm.IPAddress = '10.10.14.12'
-    ret = comm.Read(tag_list)
-
-    dir = makeDir()
-    # bookName = "Tool_Crib_Log-" + date + ".csv"
+while True:
     hour = int(time.strftime("%H"))
-    bookName = generateFilename(hour - 1)
+    dir = makeDir()
+    curr_time = str(t.tm_hour) + ":" + str(t.tm_min) + ":" + str(t.tm_sec)
 
-    # for testing 
-    for r in ret:
-        print(r.Value)
+    # Check if it's time to generate the file (every 8 hours starting at 7 am)
+    if curr_time == "7:0:0" or curr_time == "15:0:0" or curr_time == "23:0:0":
+        hour = int(time.strftime("%H"))
+        # make a new file
+        file_name = dir + '\\' + generateFilename(hour)
+        with open(file_name, 'w', newline='') as csv_file:
+            csv_file.write("curr_time, WeldReworkId, WeldReworkReasonCode\n")
 
-    comm.Close()
+    # Read from the PLC
+    with PLC() as comm:
+        comm.IPAddress = '10.10.14.12'
+        ret = comm.Read(tag_list)
 
-    if (ret[2]):
+        if curr_time == "7:00:0" or curr_time == "15:00:0" or curr_time == "23:00:0":
+            hour = int(time.strftime("%H"))
+            # make a new file
+            file_name = dir + '\\' + generateFilename(hour)
+            with open(file_name, 'w', newline='') as csv_file:
+                csv_file.write("curr_time, WeldReworkId, WeldReworkReasonCode\n")
 
-        write_data = [('WeldReworkReasonCode', reason_codes[ret[0]]),
-                    ('WeldReworkId', ret[1])]
+        if ret[2]:  # Writes out if our WeldTechlogged is True
+            print("This is my file name: %s", curr_time)
+            with open(file_name, 'a') as csv_file:
+                csv_file.write(f"{curr_time}, {ret[1]}, {reason_codes[ret[0]]}\n")
 
-        with open(bookName, 'w') as csv_file: # need to change the name of the file 
-            csv_file = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            csv_file.writerows(write_data)
-            time.sleep(1)
-    else:
-        pass
+        time.sleep(1)
